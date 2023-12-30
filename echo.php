c49a285bde602b8c4a1b9f55fe3cf1a1d49681f9
sem6703 <sem6703@yandex.ru>
2023-12-31 01:52:46 +0400

    ииь б

--- echo.php
+++
@@ -1,6 +0,0 @@
-<?php
-header ("Content-Type: text/html; charset=utf-8");
-
-echo " Very Well ";
-
-?>
--- lev5.php
+++
@@ -1,7 +0,0 @@
-<?php
-header ("Content-Type: text/html; charset=utf-8");
-
-// выход номер страницы
-if ( isset($_GET['count']))echo $_GET['count'];
-
-?>
